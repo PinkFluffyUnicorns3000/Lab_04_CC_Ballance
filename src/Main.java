156ed5619ebf5ed1a11bebfe464abf216d1c1817
benhillerich <hillerbh@email.uc.edu>
2023-06-02 15:01:46 -0400

    Commit 2 (Final)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+    int numMonths = 2;
+    double ccBalance = 5000.00;
+    double interestRate = .17;
+    System.out.println("Your original ballance was " + ccBalance + " dollars.");;
+    for(int i = 1; i <= numMonths; i++)
+    {
+        double interest = ccBalance * interestRate;
+        interest = Math.round(interest * 100) / 100.00;
+        ccBalance = ccBalance + interest;
+        System.out.println("After " + i + " months your ballance is " + ccBalance + " dollars.");
+    }
     }
 }
